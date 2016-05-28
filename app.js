970407a86712639514a35dbb0aca6189052cb046
Obi <obi.netbits@outlook.com>
2016-05-28 19:18:25 +0000

    display data

--- app.js
+++ app.js
@@ -2,12 +2,13 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
+app.set("view engine", "ejs");
 
 app.get("/results", function(req, res){
-    request("http://www.omdbapi.com/?s=california", function(error, response, body){
+    request("http://omdbapi.com/?s=california", function(error, response, body){
         if(!error && response.statusCode == 200){
-            var results = JSON.parse(body);
-            res.send(results["Search"][0]["Title"]);
+            var data = JSON.parse(body);
+            res.render("results",{data: data});
         }
     });
 });
